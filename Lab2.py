4ca4770dba820b2779c28209247104e88a05a073
Cassandra Svanbro <cassandra.svanbro@gmail>
2022-11-29 14:02:59 +0100

    update

--- Lab2.py
+++ Lab2.py
@@ -89,29 +89,43 @@ class Graph():
             self.dic_weight[vertex2][vertex1] = weight
             return self.dic_weight
 
-
-graph = Graph()
+graphs = Graph()
 graph_weight = Graph.WeightedGraph()
 
-def dijkstra(self, graph, source, cost=lambda u,v: 1):
+def djikstra(graph, source):
 
-    unvisited = [self.data['times'].keys()]
-    tent_dist = {}
+    with open(graph, 'r', encoding='utf-8') as infile:
+        graphs = Graph()
+        data = json.load(infile)
+        unvisited = list(data['times'].keys())
+        tent_dist = {}
 
-    for key in unvisited:
-        tent_dist = graph.set_vertex_value(key, float('inf'))
-    tent_dist[source] = 0
+        for i in range(len(unvisited)):
+            tent_dist[unvisited[i]] = graphs.set_vertex_value(unvisited[i], float('inf'))
+        tent_dist[source] = 0
+        print(tent_dist)
+        current = source
 
-    current = source
+        while unvisited != []:
+            for i in range(len(graphs.neighbours(current))): #loops over all nieghbours to current
+                if graphs.neighbours(current)[i] in unvisited: #checks if neighbour unvisited
+                    neighbour = str(graphs.neighbours(current)[i])
+                    weight = tent_dist[current] + data['times'][current][neighbour] #weight for nieghbour + weight current
+                    #print(tent_dist[neighbour])
+                    if weight < tent_dist[neighbour]: #if weight smaller than tentative dist
+                        tent_dist[neighbour] = graphs.set_vertex_value(neighbour, weight) #set new tent dist to weigth
+            unvisited.pop(current) #current visited so remove
+            current = min(unvisited, key=unvisited.get)
+    return tent_dist
 
-    while unvisited != []:
-        for i in range(len(graph.neigbours(current))): #loops over all nieghbours to current
-            if graph.neighbours(current)[i] in unvisited: #checks if neighbour unvisited
-                neighbour = graph.neighbours(current)[i]
-                weight = tent_dist[current] + self.data['times'][current][neighbour] #weight for nieghbour + weight current
-                if weight < tent_dist[neighbour]: #if weight smaller than tentative dist
-                    tent_dist[neighbour] = graph.set_vertex_value(neighbour, weight) #set new tent dist to weigth
-        unvisited.pop(current) #current visited
+#print(djikstra('tramnetwork.json', 'Chalmers'))
+
+print(graphs.set_vertex_value('Chalmers', 5))
+
+
+
+
+
         
 
 
