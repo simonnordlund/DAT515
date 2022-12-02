aacab33fbb2ae627b1784027a17105e0ff1aeaef
Cassandra Svanbro <cassandra.svanbro@gmail>
2022-12-02 11:34:19 +0100

    fixade inputs

--- Lab2.py
+++ Lab2.py
@@ -1,103 +1,111 @@
 import json
-
-TRAM_FILE = './tramnetwork.json'
+from itertools import groupby
 
 class Graph():
 
-    def __init__(self): #run evrey time class is called to create dictionaries
-        with open(TRAM_FILE, 'r', encoding='utf-8') as infile:
-            self.data = json.load(infile)
-            self.dic_val = {}
-            self.dic = {}
-            for outer in self.data['times']:
-                temp_list = []
-                self.dic_val[outer] = []
-                for inner in self.data['times'][outer]:
-                    temp_list.append(inner)
-                self.dic[outer] = temp_list #dictionary of all stops with list nearby stops as value
+    def __init__(self, G): #run evrey time class is called to create dictionarie
+        self._valuelist = {}
+        self._adjlist = {}
+
+        self.temp = {k: [v[1] for v in g] for k, g in groupby(sorted(G), lambda e: e[0])}
+        self._adjlist = {k: [v[1] for v in g] for k, g in groupby(sorted(G), lambda e: e[0])}
+
+        for key in self.temp:
+            for item in self.temp[key]:
+                print(self._adjlist)
+                print(item)
+                if item in self._adjlist:
+                    self._adjlist[item].append(key)
+                else:
+                    self._adjlist[item] = [key]
+        
+        for key in self._adjlist:
+            self._valuelist[key] = []
+   
             
+
+
     def neighbours(self, vertex): #find neighbours
-        return self.dic[vertex]
+        return self._adjlist[vertex]
 
     def edges(self): #gives edges
         edges = []
-        for key in self.data['times']:
-            for key2 in self.data['times'][key]:
-                if [key2, key] not in edges and [key, key2] not in edges:
-                    edges.append([key, key2])
+        for key in self._adjlist:
+            for key2 in self._adjlist[key]:
+                if (key2, key) not in edges and (key, key2) not in edges:
+                    edges.append((key, key2))
         return edges
 
     def vertices(self): #gives all nodes aka stops
         vertices = []
-        for key in self.data['stops']:
+        for key in self._adjlist:
             vertices.append(key)
         return vertices
 
     def __len__(self): #amount of stops
-        count = len(self.dic)
+        count = len(self._adjlist)
         return count
 
     def add_vertex(self, vertex): #adds vertex without neighbours
-        if vertex not in self.dic:
-            self.dic[vertex] = []
-        return self.dic
+        if vertex not in self._adjlist:
+            self._adjlist[vertex] = []
+        return self._adjlist
 
     def add_edge(self, vertex1, vertex2): #adds edge between two stops
-        if vertex1 in self.dic:
-            self.dic[vertex1].append(vertex2)
+        if vertex1 in self._adjlist:
+            self._adjlist[vertex1].append(vertex2)
         else:
-            self.dic[vertex1] = [vertex2]
-        if vertex2 in self.dic:
-            self.dic[vertex2].append(vertex1)
+            self._adjlist[vertex1] = [vertex2]
+        if vertex2 in self._adjlist:
+            self._adjlist[vertex2].append(vertex1)
         else:
-            self.dic[vertex2] = [vertex1]
-        return self.dic
+            self._adjlist[vertex2] = [vertex1]
+        return self._adjlist
     
     def remove_vertex(self, vertex): #removes stop from dictionary
-        self.dic.pop(vertex)
-        for key in self.dic:
-            for i in range(len(self.dic[key])-1):
-                if self.dic[key][i]==vertex:
-                    self.dic[key].remove(vertex)
-        return self.dic
+        self._adjlist.pop(vertex)
+        for key in self._adjlist:
+            for i in range(len(self._adjlist[key])-1):
+                if self._adjlist[key][i]==vertex:
+                    self._adjlist[key].remove(vertex)
+        return self._adjlist
 
     def remove_edge(self, vertex1, vertex2): #removes edge in dictionary
-        self.dic[vertex1].remove(vertex2)
-        self.dic[vertex2].remove(vertex1)
-        return self.dic
+        self._adjlist[vertex1].remove(vertex2)
+        self._adjlist[vertex2].remove(vertex1)
+        return self._adjlist
 
     def get_vertex_value(self, vertex): #gets value
-        return self.dic_val[vertex]
+        return self._valuelist[vertex]
 
     def set_vertex_value(self, vertex, value): #sets value
         if isinstance(vertex, list):
             for i in range(len(vertex)):
-                self.dic_val[vertex[i]] = value
+                self._valuelist[vertex[i]] = value
         else:
-            self.dic[vertex] = value
-        return self.dic_val
+            self._adjlist[vertex] = value
+        return self._valuelist
 
 
     class WeightedGraph():
 
         def __init__(self): #runs every time class called
-            with open(TRAM_FILE, 'r', encoding='utf-8') as infile:
-                self.data = json.load(infile)
-                self.dic_weight = self.data['times']
+            self._weightlist = {}
+            edges = self.edges()
+            for i in range(len(edges)):
+                self._weightlist[edges[i]] = []
 
         def get_weight(self, vertex1, vertex2): #gets weigth between two vertices 
-            return self.dic_weight[vertex1][vertex2]
+            return self._weightlist[(vertex1, vertex2)]
 
         def set_weight(self, vertex1, vertex2, weight): #sets weight
-            self.dic_weight[vertex1][vertex2] = weight
-            self.dic_weight[vertex2][vertex1] = weight
-            return self.dic_weight
+            self._weightlist[vertex1, vertex2] = weight
+            return self._weightlist
 
 
-    def dijkstra(self, source, cost): #shortest path finding algorithm
+
+    def dijkstra(self, graph, source, cost=lambda u,v: 1): #shortest path finding algorithm
         
-        #graphs = Graph()
-        #graphs_weight = Graph.WeightedGraph()
         dist = {} #dictionary of distance between stops
         prev = {} #previous stops, path
         unvisited = [] #list of unvisted stops
@@ -131,7 +139,7 @@ class Graph():
 
         dji = {} #djikstra dictionary
 
-        for target in graphs.vertices():
+        for target in self.vertices():
             S = [] #stop sequence
             u = target #start at target
 
@@ -147,23 +155,19 @@ class Graph():
 
 
 
-graphs = Graph()
-graph_weight = Graph.WeightedGraph()
+
 
 with open('tramnetwork.json', 'r', encoding='utf-8') as infile:
     data = json.load(infile)
     weight = data['times']
 
-print(graphs.dijkstra('SKF', weight)['Chalmers']['path'])
-
-
-
+#print(graphs.dijkstra('SKF', weight)['Chalmers']['path'])
 
 
 import os
 os.environ["PATH"] += os.pathsep + 'C:/Program Files/Graphviz/bin/'
 
-def visualize():
+def visualize(graph, view='dot', name='mygraph', nodecolors={}, engine='dot'):
     import graphviz as gr
     dot = gr.Graph(name = 'Graph')
     graphs = Graph()
@@ -173,21 +177,17 @@ def visualize():
     for e in range(len(edges)):
         dot.edge(edges[e][0], edges[e][1])
     dot.render(format='png', view=True)
-
-'''
+    
 def view_shortest(G, source, target, cost=lambda u,v: 1):
-    path = dijkstra(G, source, cost)[target]['path']
+    path = Graph.dijkstra(G, source, cost)[target]['path']
     print(path)
     colormap = {str(v): 'orange' for v in path}
     print(colormap)
     visualize(G, view='view', nodecolors=colormap)
 
 def demo():
-    G = ([(1,2),(1,3),(1,4),(3,4),(3,5),(3,6),(3,7),(6,7)])
+    G = Graph([(1,2),(1,3),(1,4),(3,4),(3,5),(3,6), (3,7), (6,7)])
     view_shortest(G, 2, 6)
 
 if __name__ == '__main__':
-    demo() '''
-
-
-
+    demo()
